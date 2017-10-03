409ff8cf11f21fa80730aea81a9b510fc1cc18cf
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-10-04 00:30:44 +0300

    added  photo models, and get models from service in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Model} from "./models";
+import {ModelsService} from "./models.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  models: Model[] = [];
+
+  constructor(private modelsService: ModelsService) {}
+
+  ngOnInit(){
+    this.models = this.modelsService.getAll();
+  }
 }
