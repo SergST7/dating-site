056f90de2306b9ea562408f7496ca4710f40c117
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-10-05 01:45:40 +0300

    added  showDetails method into app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Model} from "./models";
 import {ModelsService} from "./models.service";
+import {MdSidenav} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,19 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   models: Model[] = [];
+  selectedModel: Model;
+  @ViewChild('sidenav') sidenav: MdSidenav;
 
   constructor(private modelsService: ModelsService) {}
 
   ngOnInit(){
     this.models = this.modelsService.getAll();
+  }
+
+  showDetails(model: Model){
+    // 1. set selected models
+    this.selectedModel = model;
+    // 2. open sidenav
+    this.sidenav.open();
   }
 }
